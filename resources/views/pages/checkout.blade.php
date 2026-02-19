17137bf4878bee919f7277344a0a959622d396eb
Akhil-Techsoft <techsofttest@gmail.com>
2026-02-19 16:06:08 +0530

    Fix checkout login

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -47,28 +47,8 @@
   
   <div class="col-lg-8">
   <div class="woocommerce-form-login-toggle">
-      <div class="woocommerce-info">Returning customer? <a href="javascript:void(0);" class="showlogin">Click here to login</a></div>
+      <div class="woocommerce-info">Returning customer? <a href="javascript:void(0);" class="showlogin" data-bs-toggle="modal" data-bs-target="#youmyModal">Click here to login</a></div>
     </div>
-    <form action="#" method="post" class="woocommerce-form-login" style="display:none;">
-      <div class="row">
-        <div class="col-lg-12 ">
-		    <div class="row">
-          <div class="form-group col-sm-6">
-            <input type="text" class="form-control" placeholder="Username or email" required>
-          </div>
-          <div class="form-group col-sm-6">
-            <input type="text" class="form-control" placeholder="Password" required>
-          </div>
-         
-          <div class="form-group">
-            <button type="submit" class="th-btn   style3">Login</button>
-       
-			 </div>
-          </div>
-        </div>
-      </div>
-    </form>
- 
    
 	
 <form action="{{ route('checkout.confirm') }}" method="POST" class="woocommerce-checkout mt-40">
